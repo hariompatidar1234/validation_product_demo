6c6bf95b7824f270a5f03587160c533f7640640d
hariom <hariompatidar32498@gmail.com>
2023-08-14 12:30:08 +0530

    check some validtion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
 	 validates :name, presence: true
+
+	 after_initialize do |user|
+	 	puts 'you hava initialize the object'
+	 end 
+	 after_find do |use|
+	 	puts 'you hava find the obejct'
+	 end 
+	 after_touch do |user|
+	 	puts'you have touched the obejct'
+	 end 
 end
